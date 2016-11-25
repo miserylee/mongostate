573fe2ff499176f4404cd019d0a5857aff1bcdf5
Misery Lee <miserylee@foxmail.com>
2016-11-25 21:56:11 +0800

    Merge branch 'master' of https://github.com/CooooSo/mongostate into CooooSo-master
    # Conflicts:
    #	index.js

--- Example/Person.js
+++ Example/Person.js
@@ -3,7 +3,12 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
   name: String,
-  gender: String
+  gender: String,
+  profile: {
+    nickname: String
+  }
 });
+
+schema.plugin(require('mongoose-timestamp'));
 
 module.exports = require('./connection').model('person', schema);
--- Example/index.js
+++ Example/index.js
@@ -4,38 +4,99 @@ const co = require('co');
 const Transaction = require('../');
 const connection = require('./connection');
 Transaction.noWarning = true;
+const assert = require('assert');
 
 co(function * () {
   yield connection.dropDatabase();
-  const t = yield Transaction.init({
-    connection,
-    historyConnection: connection,
-    biz: {
-      operator: 'system'
-    }
-  });
-  yield t.try(function * () {
-    yield createPersonAndRecharge.bind(this)({
-      name: 'Misery',
-      gender: 'male',
-      amount: 100
-    });
-  });
-  const people = yield Person.find();
-  console.log(people);
-  const wallets = yield Wallet.find();
-  console.log(wallets);
-  const peopleHistory = yield t.use(Person).findLatestHistory();
-  console.log(peopleHistory.toJSON());
-  const revertedPerson = yield t.use(Person).revertTo(peopleHistory.id);
-  console.log(revertedPerson);
-  console.log((yield t.use(Person).findLatestHistory()).toJSON());
+
+  const person = yield createAPerson();
+
+  yield updateNickname(person.id);
+
+  yield unsetPersonGender(person.id);
+
+  yield createAWalletAndRecharge(person.id);
+
+  console.log('success!')
 }).catch(console.error);
 
-function * createPersonAndRecharge ({ name, gender, amount }) {
-  const TPerson = this.use(Person);
-  const TWallet = this.use(Wallet);
-  const person = yield TPerson.create({ name, gender });
-  const wallet = yield TWallet.create({ person: person.id });
-  yield TWallet.findByIdAndUpdate(wallet.id, { $inc: { money: amount } });
+function * createAPerson () {
+  const t = yield Transaction.init({
+    connection
+  });
+
+  const p = yield t.try(function * () {
+    const TPerson = this.use(Person);
+    return yield TPerson.create({
+      name: 'Misery',
+      gender: 'male',
+      profile: {
+        nickname: 'Mis'
+      }
+    });
+  });
+
+  const person = yield Person.findById(p.id);
+
+  assert(person);
+  assert(person.name === 'Misery');
+  assert(person.gender === 'male');
+  assert(person.profile.nickname === 'Mis');
+  return person;
+}
+
+function * updateNickname (id) {
+  const t = yield Transaction.init({ connection });
+  yield t.try(function * () {
+    const TPerson = this.use(Person);
+    yield TPerson.findByIdAndUpdate(id, {
+      $set: { 'profile.nickname': 'Luna' }
+    });
+  });
+
+  const person = yield Person.findById(id);
+  assert(person);
+  assert(person.name === 'Misery');
+  assert(person.profile.nickname === 'Luna');
+}
+
+function * unsetPersonGender (id) {
+  const t = yield Transaction.init({ connection });
+  yield t.try(function * () {
+    const TPerson = this.use(Person);
+    yield TPerson.findByIdAndUpdate(id, {
+      $unset: { gender: '' }
+    });
+  });
+
+  const person = yield Person.findById(id);
+  assert(person);
+  assert(person.name === 'Misery');
+  assert(!person.gender);
+}
+
+function * createAWalletAndRecharge (personId) {
+  const t = yield Transaction.init({ connection });
+  const w = yield t.try(function * () {
+    const TWallet = this.use(Wallet);
+    const TPerson = this.use(Person);
+
+    const person = yield TPerson.findById(personId);
+    assert(person);
+
+    const wallet = yield TWallet.create({
+      person: person.id
+    });
+
+    return yield TWallet.findByIdAndUpdate(wallet.id, {
+      $set: {
+        money: 100
+      }
+    });
+  });
+
+  const wallet = yield Wallet.findById(w.id);
+  assert(wallet);
+  assert(wallet.person.toString() === personId.toString());
+  assert(wallet.money === 100);
 }
